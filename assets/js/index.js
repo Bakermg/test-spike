dbaafee800fea1d8d476ed81f2b5e054aa92af2e
Mike Baker <bakermg@me.com>
2017-03-16 17:27:50 -0400

    Finished putting together the index page with styles and media queries, added sticky nav menu with jquery pluginand started to work on blog page.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,29 @@
-console.log('you can use ES6 here : )')
+
+$(document).ready(function() {
+
+    //jQuery plugin for sticking nav to the top off the page
+    $('#myNav').stickThis();
+
+    // active widget
+    $('#an-nyw_1').css({
+    	'width': '80%',
+    	'margin': 'auto',
+    	'margin-top': '5px'
+    });
+    $('.an-nyw-window').css({
+    	'width': '100%',
+    	'margin': 'auto'
+    });
+    $('.an-nyw_1_results').css({
+      'width': '100%',
+      'margin': 'auto'
+      // 'font-size': '1.1em'
+    })
+
+    // remove the animate classes on mobile views
+    var width = $(window).width();
+    console.log(width);
+    if (width < 767) {
+    	$('li').removeClass('hvr-float');
+    }
+});
